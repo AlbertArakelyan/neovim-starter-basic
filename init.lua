a9fb2d9955fb851325c183eed1c16dec6d8bc41a
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-03-17 12:46:41 +0400

    made setup more stable

--- init.lua
+++ init.lua
@@ -3,9 +3,12 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
 
 -- Install lazy.nvim automatically
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -19,27 +22,58 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugins
-require("lazy").setup({
+local plugins = {
     { "nvim-lua/plenary.nvim" },
 
     -- theme
-    { "folke/tokyonight.nvim", config = function()
-        vim.cmd.colorscheme("tokyonight")
-    end },
+    --{ "folke/tokyonight.nvim", config = function()
+    --    vim.cmd.colorscheme("tokyonight")
+    --end },
 
-    -- treesitter (syntax highlighting)
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+    -- telescope
+    {
+        'nvim-telescope/telescope.nvim', tag = '*',
+        dependencies = { 
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
+    },
+
+    -- treesitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate'
+    }
 
     -- LSP
-    { "neovim/nvim-lspconfig" },
+    -- { "neovim/nvim-lspconfig" },
 
     -- autocomplete
-    { "hrsh7th/nvim-cmp",
-      dependencies = {
-          "hrsh7th/cmp-nvim-lsp",
-          "L3MON4D3/LuaSnip",
-      },
-    },
+    -- { "hrsh7th/nvim-cmp",
+    --  dependencies = {
+    --      "hrsh7th/cmp-nvim-lsp",
+    --      "L3MON4D3/LuaSnip",
+    --  },
+    -- },
+}
+local opts = {}
+
+-- Plugins
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.config")
+config.setup({
+    ensure_installed = {"lua", "javascript", "rust", "zig"},
+    highlight = { enable = true },
+    indent = { enable = true }
 })
 
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
